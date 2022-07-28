6cb9d6d8b022570f7a75ccebdfb73109c95d43eb
MoonDokkebi <jsangmin99@gmail.com>
2022-07-28 12:49:39 +0900

    2022-07-28

--- src/main/java/com/example/first_springboot/entity/Article.java
+++ src/main/java/com/example/first_springboot/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.first_springboot.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 
 @Entity // DB가 해당 객체를 인식 가능
 @AllArgsConstructor
+@NoArgsConstructor//디폴트 생성자
 @ToString
 public class Article {
     @Id // 대표값을 지정!
